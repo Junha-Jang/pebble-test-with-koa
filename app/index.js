4b40a61b9d3075e6f76e179daa5eb16b347c2cdb
Sungbin Jo <pcr910303@icloud.com>
2018-10-16 08:07:53 +0900

    Mount api to /api and serve html files in ./app/view/dist on root

--- app/index.js
+++ app/index.js
@@ -1,7 +1,13 @@
 const Koa = require("koa");
+const serve = require("koa-static");
+const mount = require("koa-mount");
+
+const router = require("./route/index.js");
 
 const app = new Koa();
 const PORT = 8888;
+
+app.use(serve(__dirname + "/view/dist")).use(mount("/api", router.routes())).use(mount("/api", router.allowedMethods()));
 
 const server = app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
 
